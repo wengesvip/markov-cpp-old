dfda7b65e24bc91c66d398d5d29ea2b6d92a549c
wengesvip <46880981+wengesvip@users.noreply.github.com>
2019-09-21 21:19:47 +0800

    Add files via upload

---
+++ mcpp_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef MCPP_PRIVATE_H
+#define MCPP_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"3.0.0.1"
+#define VER_MAJOR	3
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	"3.0.0.1"
+#define FILE_DESCRIPTION	"A C++ program that generate text based on input data."
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"markov-cpp"
+#define PRODUCT_VERSION	"3.0.0.1"
+
+#endif /*MCPP_PRIVATE_H*/
